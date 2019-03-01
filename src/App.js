9a446bc4df52f7d0c5fc892e35130095f725b7a2
Niklas Ek <nikek@spotify.com>
2019-03-01 16:25:17 +0100

    [style] basic layout layed out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import { ReactComponent as Logo } from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <nav className="nav">
+          <Logo className="app-logo" alt="logo" />
+        </nav>
+        <main className="main">
+          <h1 className="page-title">Dashboard</h1>
+        </main>
+        <nav className="user">
+          <div className="balance">Balance</div>
+          <div className="account">
+            <div className="circle" />
+          </div>
+        </nav>
+        <aside className="detail">
+          <h2>Recent Transactions</h2>
+        </aside>
       </div>
     );
   }
