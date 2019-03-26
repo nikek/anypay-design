ca25d4b7d7fa691cd079129dcc44b25750b264bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-26 22:09:47 +0000

    Deploy nikek/anypay-design to github.com/nikek/anypay-design.git:gh-pages

---
+++ precache-manifest.139423402d42f9753da72a5167e9bac5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32c44d70a3ce04e4ceb",
+    "url": "/anypay-design/static/css/main.9cb2abe6.chunk.css"
+  },
+  {
+    "revision": "b32c44d70a3ce04e4ceb",
+    "url": "/anypay-design/static/js/main.b32c44d7.chunk.js"
+  },
+  {
+    "revision": "8671d87cd77931c609df",
+    "url": "/anypay-design/static/js/runtime~main.8671d87c.js"
+  },
+  {
+    "revision": "73aa82c4b396619ca61e",
+    "url": "/anypay-design/static/js/2.73aa82c4.chunk.js"
+  },
+  {
+    "revision": "6c738e4eebe9b8bfda9e32bab7b8984a",
+    "url": "/anypay-design/static/media/welcome-bg.6c738e4e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anypay-design/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f982bb47d1b0cf2f95b4a9300cd90aaa",
+    "url": "/anypay-design/static/media/user.f982bb47.svg"
+  },
+  {
+    "revision": "550a7cf66e25a0876df6689a8aef2942",
+    "url": "/anypay-design/static/media/folder.550a7cf6.svg"
+  },
+  {
+    "revision": "440c1a442155e440a5507545f8dbb118",
+    "url": "/anypay-design/static/media/upload.440c1a44.svg"
+  },
+  {
+    "revision": "352a4fb737d401e5be953acf1010c04f",
+    "url": "/anypay-design/static/media/menu.352a4fb7.svg"
+  },
+  {
+    "revision": "29105add182fff2bc412528df2ca437b",
+    "url": "/anypay-design/static/media/dollar-sign.29105add.svg"
+  },
+  {
+    "revision": "9b699062190476641a280f6046c929fe",
+    "url": "/anypay-design/static/media/credit-card.9b699062.svg"
+  },
+  {
+    "revision": "0e2be8a3f41b537bebfdab20c318876d",
+    "url": "/anypay-design/static/media/bar-chart.0e2be8a3.svg"
+  },
+  {
+    "revision": "cd8e4aa519c648e06b2d13f8bdde2d49",
+    "url": "/anypay-design/static/media/x.cd8e4aa5.svg"
+  },
+  {
+    "revision": "f30c272338bc3fa5c245cdba9443707a",
+    "url": "/anypay-design/static/media/search.f30c2723.svg"
+  },
+  {
+    "revision": "66458eb5216f6aef58247e8a6bf834fb",
+    "url": "/anypay-design/static/media/sliders.66458eb5.svg"
+  },
+  {
+    "revision": "c4056675e98cfeec62e47edd4ea2cffc",
+    "url": "/anypay-design/index.html"
+  }
+];
